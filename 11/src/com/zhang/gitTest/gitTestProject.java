b8fa34faf6a268dfccc708beab1cb1d692179a3b
zhanghao <1468872656@qq.com>
2020-08-06 11:07:03 +0800

    本地工程添加到本地仓库

---
+++ 11/src/com/zhang/gitTest/gitTestProject.java
@@ -0,0 +1,9 @@
+package com.zhang.gitTest;
+
+public class gitTestProject {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
